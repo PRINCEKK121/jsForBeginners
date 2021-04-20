7a8f178278f5fbee9a38334cb59dddff5a296ae5
PRINCEKK121 <prince.karikari@amalitech.org>
2021-04-20 04:22:55 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const filterResults = document.querySelector('#filterResults');
+
+const displayResults = (birthYear, dob, year, age) => {
+  
+  filterResults.innerHTML = `
+    <li>Your birth year is: ${birthYear}</li>
+    <li>Your date of birth is: ${dob.toDateString()}</li>
+    <li>You were ${age} years in ${year}</li>
+  `;
+
+  filterResults.style.display = 'block';
+};
+
+const calculate = () => {
+  const number = Number(document.querySelector('#number').value);
+  let dob = new Date(document.querySelector('#dateOfBirth').value);
+  let ageInAYear = document.querySelector('#ageAtDate').value;
+
+  const isValidEntries =
+    number <= 0 || number === '' || dob === '' || ageInAYear === '';
+
+  if (isValidEntries) {
+    alert('Please make sure all the entered is valid!');
+    return;
+  }
+
+  console.log('Inside calculate method');
+  const birthYear = new Date().getFullYear() - number;
+
+  const year = new Date(ageInAYear).getFullYear();
+  ageInAYear = year - new Date(dob).getFullYear();
+
+  displayResults(birthYear, dob, year, ageInAYear);
+};
+
+filterResults.style.display = 'none';
+
+document.querySelector('#button').addEventListener('click', calculate);
